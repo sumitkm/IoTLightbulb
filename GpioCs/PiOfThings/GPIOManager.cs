466ed2dd513e7fd827d806e183e3aba824e2686e
sumitkm@gmail.com <sumitkm@gmail.com>
2015-03-22 23:08:20 +0000

    Updated solution to point to PiOfThings project

--- GpioCs/PiOfThings/GPIOManager.cs
+++ GpioCs/PiOfThings/GPIOManager.cs
@@ -9,7 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
-namespace GpioCs
+namespace PiOfThings
 {
 	public class GPIOManager
 	{
--- GpioCs/PiOfThings/GPIOPinReferences.cs
+++ GpioCs/PiOfThings/GPIOPinReferences.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace GpioCs
+namespace PiOfThings
 {
 	public static class GPIOPinDirection
 	{
